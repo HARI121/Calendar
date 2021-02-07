2d3654e5d2a0846a44f84e07d279a3b6920115aa
vishnumd91 <vishnudivakaran1@gmail.com>
2021-02-07 13:32:21 +0530

    Event feauture added

--- src/index.js
+++ src/index.js
@@ -1,19 +1,5 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {render} from 'react-dom';
-import Calendar from 'react-calendar';
+import App from './App';
 
-const ReactCalendar = () => {
-  const [date, setDate] = useState(new Date());
-
-  const onChange = date =>{
-    setDate(date);
-  };
-  return <div>
-      <Calendar
-        onChange={onChange}
-        value={date}/>
-        {console.log(date)}
-      {date.toString()}
-  </div>
-};
-render(<ReactCalendar/>, document.querySelector("#root"));
+render(<App />, document.querySelector("#root"));
